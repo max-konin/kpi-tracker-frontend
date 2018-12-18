23f48219ccaaed79fd55bf441c87fd793e42fc3e
Max Konin <maxim21214@gmail.com>
2018-12-18 12:46:31 +0700

    Added filter by finishedat to the tasks page

--- app/routes/dashboard/tasks.js
+++ app/routes/dashboard/tasks.js
@@ -1,7 +1,13 @@
 import Route from '@ember/routing/route';
+import { buildFinishedAtFilter } from 'ember-starter-project/utils/task-filter-builder';
 
 export default Route.extend({
-  model() {
-    return this.store.findAll('task');
+  queryParams: {
+    lastDays: { refreshModel: true }
+  },
+
+  model(params) {
+    const filter = buildFinishedAtFilter(params.lastDays);
+    return this.store.query('task', { q: filter });
   }
 });
