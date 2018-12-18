f1cb19f1bbdde75e907d773bee29ef7829fd7c36
Max Konin <maxim21214@gmail.com>
2018-12-18 16:13:14 +0700

    Bugfix. fix params for ransack

--- app/utils/task-filter-builder.js
+++ app/utils/task-filter-builder.js
@@ -1,4 +1,4 @@
 import moment from 'moment';
 
-const buildDate = daysAgo => moment().subtract(daysAgo, 'd').startOf('day').toDate();
-export const buildFinishedAtFilter = daysAgo => ({ task_finished_at_gte: buildDate(daysAgo) });
+const buildDate = daysAgo => moment().subtract(daysAgo, 'd').startOf('day').toISOString();
+export const buildFinishedAtFilter = daysAgo => ({ task_finished_at_gteq: buildDate(daysAgo) });
--- tests/unit/utils/task-filter-builder-test.js
+++ tests/unit/utils/task-filter-builder-test.js
@@ -6,8 +6,8 @@ import moment from 'moment';
 describe('Utils | TaskFilterbuilder', function () {
   describe('#buildFinishedAtFilter', function () {
     it('builds filter for taskFinishedAt field with gte options', function () {
-      const expectedDate = moment().subtract(7,'d').startOf('day').toDate()
-      expect(buildFinishedAtFilter(7)).to.deep.eq({ task_finished_at_gte: expectedDate })
+      const expectedDate = moment().subtract(7,'d').startOf('day').toISOString()
+      expect(buildFinishedAtFilter(7)).to.deep.eq({ task_finished_at_gteq: expectedDate })
     });
   });
 });
